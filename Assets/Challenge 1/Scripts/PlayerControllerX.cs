abbcb6bb739b4a5f8f4d4b39a870f9bc4939ae48
HBFL3Xx <happybanda@dyuni.ac.mw>
2024-09-15 17:02:25 +0200

    fix: add a manageable speed to the plane
    Modified the speed variable of the player controller to a medium controllable speed

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
+    private float speed = 8.0f;
     public float rotationSpeed;
     public float verticalInput;
 
